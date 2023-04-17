26886652241ffc8b0e4ac80e326512cf6b98709d
geigeralfredo <alfredogeiger@gmail.com>
2023-04-17 18:17:17 -0300

    Checked if parameter sizes are zero

--- JCS_selectBooks/JCS_CreateAllBooks.sh
+++ JCS_selectBooks/JCS_CreateAllBooks.sh
@@ -20,13 +20,28 @@ for i in "${!MAPFILE[@]}";
 do  
   case "$i" in
         0)  BookCollection=${MAPFILE[0]}
+            if [ ${#BookCollection} == 0 ]; then
+              echo "$scriptName - parameter 'BookCollection' has no information."
+              echo "$scriptName - Script will terminate."
+              exit 1
+            fi
             echo "${MAPFILE[0]}"
            ;;
         1)  BookLists=${MAPFILE[1]}
+        if [ ${#BookLists} == 0 ]; then
+              echo "$scriptName - parameter 'BookLists' has no information."
+              echo "$scriptName - Script will terminate."
+              exit 1
+            fi
             echo "${MAPFILE[1]}"
            ;;
         2)  create_y_n=${MAPFILE[2]}
-            echo "${MAPFILE[1]}"
+            if [ ${#create_y_n} == 0 ]; then
+              echo "$scriptName - parameter 'create_y_n' has no information."
+              echo "$scriptName - Script will terminate."
+              exit 1
+            fi
+            echo "${MAPFILE[2]}"
            ;;
         *)      echo "$scriptName - Three (3) arguments must be informed:"
                 echo "$scriptName - - 1) The book collection Diretory, plus"
